3f889845239e6de4cd8d3ee266845b6d187fd387
Bhavani Goruganthu <bhavani.goruganthu@mail.sfsu.edu>
2021-02-20 16:06:31 -0800

    Milestone 1 Issues 1 & 2

--- client.py
+++ client.py
@@ -1,14 +1,25 @@
+import sys # to accept commandline arguments
 import socket  # used to send and receive data between endpoints
-
+HOST = sys.argv[1]
+PORT = int(sys.argv[2])
 # create a socket object
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # AF_INET similar to ipv4, SOCK_STREAM represents TCP
+ # AF_INET similar to ipv4, SOCK_STREAM represents TCP
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# connect to the host and port the server socket is on
+client.connect((HOST, PORT))
 
-# connect to the port 1234
-s.connect((socket.gethostname(),1234))
+name = input("Enter your Name: ")
+client.sendall(bytes(name, 'utf-8'))
 
 # receive data from the server
-msg= s.recv(1024)  # buffer size
+msg= client.recv(1024)  # buffer size
 print(msg.decode("utf-8")) # specify encoding
 
-# close the connection  
-s.close()  
+message = input(" -> ")  # take input
+while message.lower().strip() != 'bye':
+    client.send(message.encode())  # send message
+    data = client.recv(1024).decode()  # receive response
+    print('Received from Server: ' + data)  # show in terminal
+    message = input(" -> ")  # again take input
+    # print(message.lower().strip())
+client.close()
--- server.py
+++ server.py
@@ -1,16 +1,35 @@
+import sys # to accept commandline arguments
 import socket  # used to send and receive data between endpoints
-
+HOST = sys.argv[1]
+PORT = int(sys.argv[2])
 # create a socket object
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # AF_INET similar to ipv4, SOCK_STREAM represents TCP
+# AF_INET similar to ipv4, SOCK_STREAM represents TCP
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+print('Socket Created')
+# bind to the host & port passed in the commandline
+server.bind((HOST,PORT))
+server.listen(2) # listen to a connection
+print('Waiting for Connection')
 
-# reserve a port 1234 & bind to it
-s.bind((socket.gethostname(),1234))
-s.listen(5) # listen to any connections
-
-# until an interruption
 while True:
-    clientsocket, address = s.accept() # establish connection
-    print(f"Connction successful! Address: {address}")
-    clientsocket.send(bytes("Hello..", "utf-8"))
-    # close the connection  
-    clientsocket.close()
+    try:
+        connection, address = server.accept() # establish connection
+        name = connection.recv(1024).decode()
+        print(f"Connection successful! Address: {address} Name: {name}")
+        connection.send(bytes("Hello..", "utf-8"))
+        while True:
+            # receive data stream
+            data = connection.recv(1024).decode()
+            if not data:
+                # if data is not received break
+                break
+            print("From connected User: " + str(data))
+            data = input(' -> ')
+            connection.send(data.encode())  # send data to the client
+        connection.close(); server.close();break
+    except KeyboardInterrupt:
+        print(f"closing connection to {address}.")
+        connection.shutdown(socket.SHUT_RDWR)
+        connection.close()
+        sys.exit()
