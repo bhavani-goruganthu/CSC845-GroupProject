1dd7816c18c12b175414df22dc3c829269f14eb5
Justin T. Sampson <justin@krasama.com>
2021-03-05 15:04:01 -0800

    Integrate ChatUI into client.
    Fixes #5

--- client.py
+++ client.py
@@ -1,6 +1,7 @@
 import sys # to accept commandline arguments
 import socket  # used to send and receive data between endpoints
 import m1proto
+from chatui import ChatUI
 
 HOST = sys.argv[1]
 PORT = int(sys.argv[2])
@@ -13,18 +14,17 @@ print("Connected to the Server Socket..!!")
 
 proto = m1proto.M1Protocol(client)
 
-while True:
-    try:
-        print("Enter Something: ")
-        message = input(" -> ")  # take input
-        while message.lower().strip() != 'bye':
+try:
+    with ChatUI() as ui:
+        message = ui.get_input()
+        while message != None:
             proto.send(message)
             response = proto.recv()
-            print('Received from Server: ' + response)  # show in terminal
-            message = input(" -> ")  # again take input
-        client.close()
-    except KeyboardInterrupt:
-        print(f"Closing connection to {HOST}.")
-        client.shutdown(socket.SHUT_RDWR)
-        client.close()
-        sys.exit()
+            ui.add_output(response)
+            message = ui.get_input()
+except KeyboardInterrupt:
+    pass
+finally:
+    print(f"Closing connection to {HOST}.")
+    client.shutdown(socket.SHUT_RDWR)
+    client.close()
