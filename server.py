9046f2c91f1e21314ce02db2765b1fe5cca9e3f2
Justin T. Sampson <justin@krasama.com>
2021-04-04 14:44:24 -0700

    Require users to log in #9

--- server.py
+++ server.py
@@ -22,26 +22,43 @@ server.bind((HOST,PORT))
 print('Waiting for Connection')
 server.listen(5) # listen to connections
 
+
+def receive_login(connection):
+    while True:
+        username_msg = m2proto.recv(connection)
+        if not username_msg or username_msg[0] != 8:
+            return False
+        password_msg = m2proto.recv(connection)
+        if not password_msg or password_msg[0] != 9:
+            return False
+        if password_msg[1] == username_msg[1][::-1]:
+            m2proto.send(connection, 10, "")
+            return True
+        else:
+            m2proto.send(connection, 11, "")
+
+
 def client_thread(connection, address):
     try:
-        while True:
-            data = m2proto.recv(connection)
-            if data is not None:
-                (msg_type , payload) = data
-                if payload == '':
-                    break # if payload is empty break
-                print(f"From connected Client {address}): " + str(payload))
-                # broadcast msg to all clients
-                for single_client in clients:
-                    m2proto.send(single_client, 0, payload)
-            else:
-                break
+        if receive_login(connection):
+            clients[connection] = address[1]  # store the connection object and the address
+            while True:
+                data = m2proto.recv(connection)
+                if data is not None:
+                    (msg_type , payload) = data
+                    print(f"From connected Client {address}): " + str(payload))
+                    # broadcast msg to all clients
+                    for single_client in clients:
+                        m2proto.send(single_client, 0, payload)
+                else:
+                    break
     finally:
         try:
             connection.shutdown(socket.SHUT_RDWR)
             connection.close()
         except:
             pass
+
 
 try:
     while True:
@@ -50,7 +67,6 @@ try:
         # mark each client thread as daemon so that it exits when the main program exits
         client_thread_obj = Thread(target=client_thread, args=(connection, address),  daemon=True)
         client_thread_obj.start()
-        clients[connection]= address[1] # store the connection object and the address
         threadCount +=1
         print('Thread Number: ' + str(threadCount))
 except KeyboardInterrupt:
