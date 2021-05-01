8fd608313c45d1cc4bc650370e73e6d5185f5e1e
Justin T. Sampson <justin@krasama.com>
2021-05-01 16:31:43 -0700

    Starting on file transfer

--- chatui_demo.py
+++ chatui_demo.py
@@ -6,11 +6,11 @@ try:
     with ChatUI() as ui:
         def do_output():
             while not ui.wait_for_exiting(timeout = 2):
-                ui.add_output("Testing...")
+                ui.add_output("timer", "Testing...")
         def do_input():
             line = ui.get_input()
-            while line != None:
-                ui.add_output(line)
+            while line is not None:
+                ui.add_output("you", line)
                 line = ui.get_input()
         Thread(target = do_output, daemon = True).start()
         Thread(target = do_input, daemon = True).start()
--- client.py
+++ client.py
@@ -1,7 +1,7 @@
 import sys
 import socket  # used to send and receive data between endpoints
 import ssl # wrapper for socket objects - TLS encryption
-from threading import Thread
+from threading import Thread, Lock
 from chatui import ChatUI
 import m2proto
 from getpass import getpass
@@ -34,18 +34,65 @@ if TLS:
     cert = client.getpeercert()
     print(cert)
 
+file_to_send = None
+client_lock = Lock()
+
+
 def receive():
+    global file_to_send
+    prefix = None
     response = m2proto.recv(client)
-    while response != None: # receive and print responses from the server (can be many)
+    while response is not None: # receive and print responses from the server (can be many)
         (msg_type, payload) = response
         if msg_type == 13:
-            ui.set_prefix(payload)
+            prefix = payload
         elif msg_type == 0:
-            ui.add_output(payload)
-            ui.set_prefix(None)
+            ui.add_output(prefix, payload)
+            prefix = None
+        elif msg_type == 16:
+            ui.add_output(None, "Sending file...")
+            with client_lock:
+                fd = file_to_send
+                file_to_send = None
+            Thread(target=send_file, daemon=True, args=(fd,)).start()
+        elif msg_type == 17:
+            ui.add_output(None, "Cannot send file.")
+            with client_lock:
+                if file_to_send is not None:
+                    file_to_send.close()
+                    file_to_send = None
         response = m2proto.recv(client)
     ui.send_exit_signals()
 
+
+def send_file(fd):
+    with fd:
+        b = fd.read(4096)
+        while b:
+            with client_lock:
+                m2proto.send(client, 1, b)
+            b = fd.read(4096)
+        m2proto.send(client, 18, b'')
+
+# TODO: 14+
+#     1 = file chunk
+#    14 = file target/source username
+#    15 = file name
+#    16 = OK to send
+#    17 = not OK to send
+#    18 = file complete
+# TODO: need binary support in m2proto
+# TODO: start sending
+# TODO: OK to send (from server)
+# TODO: Not OK to send (from server)
+# TODO: on server, close connection if chunk is received when shouldn't be
+# TODO: continue sending
+# TODO: receive/save
+# TODO: progress updates
+# TODO: make sure chat messages can send at same time (to test, add delay between chunks)
+# TODO: when testing/demoing, make sure each client is run from its own directory
+# TODO: when receiving, refuse if file already exists; maybe ack should go all the way to receiver and back
+#       -> open with mode "xb" and catch FileExistsError
 
 def login():
     while True:
@@ -78,15 +125,38 @@ def login():
             return False
 
 
+def handle_command(command):
+    global file_to_send
+    parts = command.split()
+    if len(parts) == 1 and (parts[0] == "q" or parts[0] == "quit"):
+        raise KeyboardInterrupt
+    elif len(parts) == 3 and parts[0] == "send":
+        recipient = parts[1]
+        filename = parts[2]
+        try:
+            fd = open(filename, "rb")
+        except PermissionError:
+            ui.add_output(None, "Cannot open file.")
+        else:
+            file_to_send = fd
+            m2proto.send(client, 14, recipient)
+            m2proto.send(client, 15, filename)
+    else:
+        ui.add_output(None, "Unrecognized command.")
+
+
 try:
     if login():
         with ChatUI() as ui:
             receive_thread = Thread(target=receive, daemon=True)
             receive_thread.start() # start the receive thread
             message = ui.get_input() # get input from user
-            while message != None:
-                if not m2proto.send(client, 0, message): # send msgs from user to the server
-                    break
+            while message is not None:
+                with client_lock:
+                    if message[0] == "/":
+                        handle_command(message[1:])
+                    elif not m2proto.send(client, 0, message): # send msgs from user to the server
+                        break
                 message = ui.get_input()
 except KeyboardInterrupt:
     pass
